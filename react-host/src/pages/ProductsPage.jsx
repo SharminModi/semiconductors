6c5bbb941682df8f6e150bd4e5175c986a8f7a5c
SharminModi <75900948+SharminModi@users.noreply.github.com>
2025-11-19 16:28:05 +0530

    Create ProductsPage.jsx

---
+++ react-host/src/pages/ProductsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductsMegaMenu from '../components/ProductsMegaMenu'
+
+export default function ProductsPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-4">Products</h1>
+      <ProductsMegaMenu />
+    </div>
+  )
+}
