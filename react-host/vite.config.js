2ffc61a85595cb2df1246b72912c3d57545bfc54
SharminModi <75900948+SharminModi@users.noreply.github.com>
2025-11-19 19:26:16 +0530

    Update base path comment in vite.config.js

--- react-host/vite.config.js
+++ react-host/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/semiconductors/', // IMPORTANT: replace with your repo name
+  base: '/semiconductors/',   // GitHub Pages base path
 })
